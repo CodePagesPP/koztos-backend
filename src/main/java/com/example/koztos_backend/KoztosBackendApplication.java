ea8eff1b5ad42d8081ef4482790c5be2b8b49072
Espinozaje <jeisson12aaron@gmail.com>
2026-02-17 10:55:46 -0500

    deploy 17/02

--- src/main/java/com/example/koztos_backend/KoztosBackendApplication.java
+++ src/main/java/com/example/koztos_backend/KoztosBackendApplication.java
@@ -1,7 +1,10 @@
 package com.example.koztos_backend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class KoztosBackendApplication {
@@ -10,4 +13,10 @@ public class KoztosBackendApplication {
 		SpringApplication.run(KoztosBackendApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init() {
+
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Lima"));
+		System.out.println("Zona horaria configurada a: " + TimeZone.getDefault().getID());
+	}
 }
